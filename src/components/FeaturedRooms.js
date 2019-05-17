58f480da57efe14a707f004946e9d2684787767b
hashmat2526 <hashmatajalali@gmail.com>
2019-05-17 12:47:37 +0500

    settle up Room component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -7,6 +7,7 @@ export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     let { loading, featuredRooms: rooms } = this.context;
+
     rooms = rooms.map(room => {
       return <Room key={room.id} room={room} />;
     });
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <div>hello from the single room</div>;
   }
