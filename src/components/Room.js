90f591e7bf1e83f5a8198365cbc9fbdaeb9e4d26
hashmat2526 <hashmatajalali@gmail.com>
2019-05-17 11:50:09 +0500

    uploaded featured room images into the home page

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Room() {
-  return <div>hello from room</div>;
+export default function Room({ room }) {
+  const { name, slug, images, price } = room;
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0]} alt="single-room" />
+      </div>
+    </article>
+  );
 }
