4077e63883e048fb567dbff655da53ed2b840f41
hashmat2526 <hashmatajalali@gmail.com>
2019-05-17 16:13:41 +0500

    settled up styled components and single room banner done

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,57 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import { RoomContext } from "../context";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
+    // console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    };
   }
-
+  // componentDidMount(){} same access to props
+  // now access context
+  static contextType = RoomContext;
   render() {
-    return <div>hello from the single room</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found ... </h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to Rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+    return (
+      <div>
+        <StyledHero img={images[0] || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to Rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+      </div>
+    );
   }
 }
