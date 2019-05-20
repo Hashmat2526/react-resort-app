2cae025a00e618e97ba43f0626e022dfd9cd7200
hashmat2526 <hashmatajalali@gmail.com>
2019-05-20 12:16:56 +0500

    single room page is finished

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
 import { RoomContext } from "../context";
-import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import StyledHero from "../components/StyledHero";
@@ -60,6 +59,31 @@ export default class SingleRoom extends Component {
               return <img key={index} src={item} alt={name} />;
             })}
           </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: ${price}</h6>
+              <h6>size: ${size} SQFT</h6>
+              <h6>
+                max capacity:
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>-{item}</li>;
+            })}
+          </ul>
         </section>
       </>
     );
